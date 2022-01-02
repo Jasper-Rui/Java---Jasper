8a39506cc115e630f09101d9663384a293b8cd95
Haorui Yang <83082501+Jasper-Rui@users.noreply.github.com>
2022-01-01 23:34:51 -0400

    Update MyDoubleLinkedList.java

--- MyLinkedList/MyDoubleLinkedList.java
+++ MyLinkedList/MyDoubleLinkedList.java
@@ -10,26 +10,88 @@ class Node {
     public int val;
     public Node next;
     public Node prev;
+
     public Node (int val) {
         this.val = val;
     }
 }
 public class MyDoubleLinkedList {
+    public Node head;
+    public Node last;
     //add first
-    public void addFirst(int data){}
+    public void addFirst(int data){
+        Node node = new Node(data);
+        if(size() == 0) {
+            this.head = node;
+            this.last = node;
+        }
+        else {
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+    }
+
     //add last
-    public void addLast(int data){}
+    public void addLast(int data){
+        Node node = new Node(data);
+        if(size() == 0) {
+            this.head = node;
+            this.last = node;
+        }
+        else{
+            this.last.next = node;
+            node.prev = this.last;
+            this.last = node;
+        }
+    }
+
     //insert at index
-    public boolean addIndex(int index,int data){}
+    public boolean addIndex(int index,int data){
+        if(size() == 0) return false;
+        return true;
+    }
+
     //check key
-    public boolean contains(int key){}
+    public boolean contains(int key){
+        Node node = this.head;
+
+        while (node != null) {
+            if(node.val == key) {
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
     //delete key
     public void remove(int key){}
+
     //remove all keys
     public void removeAllKey(int key){}
-    //get set
-    public int size(){}
-    public void display(){}
-    public void clear(){}
+
+    //get size
+    public int size(){
+        int count = 0;
+        Node node = this.head;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+        return count;
+    }
+
+    public void display(){
+        Node node = this.head;
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public void clear(){
+
+    }
 
 }
